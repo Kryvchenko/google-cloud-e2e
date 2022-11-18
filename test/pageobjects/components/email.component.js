ce93cf6f974a7b011ba13ef32e56036593099356
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-11-18 20:11:03 +0100

    dubuging

--- test/pageobjects/components/email.component.js
+++ test/pageobjects/components/email.component.js
@@ -7,9 +7,6 @@ class EmailComponent {
     get emailBtn() {
         return $("//button[@id='email_quote']");
     }
-    // get emailField() {
-    //     return $("#dialogContent_670 > form > md-content > div:nth-child(3) > md-input-container > input")
-    // }
     get emailField() {
         return $("form > md-content > div > md-input-container > input[type='email']");
     }
