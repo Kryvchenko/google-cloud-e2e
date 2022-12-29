0b7a9fd8e9a6fb8d2b2f6d558b0d0c6997cbd2b7
Oleksandr Kryvchenko <amzkryvchenko@gmail.com>
2022-12-29 22:14:41 +0100

    header-component selectors refactoring

--- test/pageobjects/components/header.component.js
+++ test/pageobjects/components/header.component.js
@@ -1,24 +1,25 @@
-const utils = require('../../utils/utils');
+const utils = require("../../utils/utils");
 class HeaderComponent {
-
-    get seacrhInput() {
-      return $("input[placeholder='Search']");
-    }
-    get linkToCalculator() {
-      return $("//div[@class='gs-title']//b[contains(text(),'Google Cloud Pricing Calculator')]");
-    }
-    get confirmCookies() {
-      return $("/html/body/devsite-snackbar/div/div/button");
-    }
-  
-    async addSearchParams () {
-      await this.confirmCookies.click();
-      await utils.doClick(this.seacrhInput);
-      await utils.doSetValue(this.seacrhInput,'Google Cloud Platform Pricing Calculator');
-      await utils.hitEnter();
-      await utils.doClick(this.linkToCalculator);
-    }
+  get seacrhInput() {
+    return $(".devsite-searchbox input");
   }
-  
-  module.exports = new HeaderComponent();
-  
+  get linkToCalculator() {
+    return $("//b[text()='Google Cloud Pricing Calculator'][1]");
+  }
+  get confirmCookies() {
+    return $(".devsite-snackbar-action");
+  }
+
+  async addSearchParams() {
+    await this.confirmCookies.click();
+    await utils.doClick(this.seacrhInput);
+    await utils.doSetValue(
+      this.seacrhInput,
+      "Google Cloud Platform Pricing Calculator"
+    );
+    await utils.hitEnter();
+    await utils.doClick(this.linkToCalculator);
+  }
+}
+
+module.exports = new HeaderComponent();
